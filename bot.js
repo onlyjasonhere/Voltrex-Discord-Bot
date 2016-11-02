2482bbae8cc7f692d8dc4d1ee2c0d6643dfa0daa
Beta <betajason1109@gmail.com>
2016-11-01 22:56:40 -0500

    Tell me if this doesn't work lol

---
+++ bot.js
@@ -0,0 +1,24 @@
+var Discord = require("discord.js");
+var bot = new Discord.Client();
+var prefix = "?" //You can change this to a prefix you like but, PLEASE DON'T USE "!"
+
+bot.on("ready", () => {
+  console.log("Bot is online and ready on " + bot.guilds.size + " servers!");
+});
+
+bot.on("message", msg => {
+
+  channel = msg.channel;
+  author = msg.author;
+  server = msg.guild;
+
+  if (msg.content.startsWith(prefix + "ping")) {
+    channel.sendMessage("Do I have to say pong?");
+  }
+
+  if (msg.content.startsWith(prefix + "whoami")) {
+    channel.sendMessage("You're " + msg.author.username + "!")
+  }
+});
+
+bot.login("BOT-TOKEN-HERE");
