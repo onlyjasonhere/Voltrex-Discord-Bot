784b33803cd2cf5785b3cbbf3b34d5fa5560d5a7
Beta <betajason1109@gmail.com>
2016-11-07 00:21:32 -0600

    Cleaned code aswell

--- commands/fun.js
+++ commands/fun.js
@@ -1,13 +1,13 @@
 var command = {}
 
 command.coinflip = {
-  "name":"coinflip",
-  "usage":"coinflip",
-  "description":"Coinflip command",
-  "process":function(bot,msg,env){
-    let coin = Math.random() > 0.5 ? 'Heads' : 'Tails'
-	msg.channel.sendMessage("The coin landed on " + `${coin}!`);
-  }
+    "name": "coinflip",
+    "usage": "coinflip",
+    "description": "Coinflip command",
+    "process": function(bot, msg, env) {
+        let coin = Math.random() > 0.5 ? 'Heads' : 'Tails'
+        msg.channel.sendMessage("The coin landed on " + `${coin}!`);
+    }
 }
 
 module.exports = command
