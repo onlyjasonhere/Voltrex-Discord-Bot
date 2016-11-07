a452d6e2a642bb028942d0ec8016c0dff4bf50a0
Joseph <joebean13@outlook.com>
2016-11-07 17:44:48 +0000

    Made it so you can choose prefix from command line when starting

--- bot.js
+++ bot.js
@@ -1,6 +1,18 @@
 var Discord = require("discord.js");
 var bot = new Discord.Client();
+
+if(args.prefix){
+  var prefix = args.prefix
+}else{
+  if(process.env.BOT_PREFIX){
+var prefix = process.env.BOT_PREFIX
+  }else{
+
+
 var prefix = "v!" //You can change this to a prefix you like but, PLEASE DON'T USE "!"
+
+}
+
 var general = require("./commands/general.js")
 var admin = require("./commands/admin.js")
 var args = require("optimist").argv
