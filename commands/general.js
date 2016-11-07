9f070199e8a1d49d63490b00d8ad8f25827bf94b
Beta <betajason1109@gmail.com>
2016-11-07 00:18:28 -0600

    Added Userinfo

--- commands/general.js
+++ commands/general.js
@@ -6,12 +6,33 @@ command.ping = {
   "name":"ping",
   "usage":"ping",
   "description":"Ping Pong command",
-  "process":function(bot,msg,env){
+  "process":function(bot, msg, env) {
     var now = Date.now()
-    msg.channel.sendMessage("Pinging...").then(function(message){
+    msg.channel.sendMessage("Pinging...").then(function(message) {
       var end = Date.now()
-      message.edit("Pong! `"+(end - now)+"ms`")
+      message.edit("Pong! `" + (end - now) + "ms`")
     })
+  }
+}
+
+command.userinfo = {
+  "name": "userinfo",
+  "usage": "userinfo @user",
+  "description": "A command to either get information about yourself or, about the mentioned member.",
+  "process":function(bot, msg, env) {
+    if (!msg.mentions.users.first()) {
+      user = msg.author;
+    } else {
+      user = msg.mentions.users.first()
+    }
+    msg.channel.sendMessage("__" + user.username + "'s Information__"
+    + "\n" + "__Name__ : " + user.username
+    + "\n" + "__User ID__ : " + user.id
+    + "\n" + "__Discriminator__ : " + user.discriminator
+    + "\n" + "__Is A Bot__ : " + user.bot
+    + "\n" + "__Date Created__ : " + user.createdAt
+    + "\n" + "__Status__ : " + user.presence.status
+    + "\n" + "__Avatar__ : " + user.avatarURL);
   }
 }
 
@@ -19,35 +40,35 @@ command.help = {
   "name":"help",
   "usage":"help",
   "description":"Sends you list of commands and help",
-  "process":function(bot,msg,env){
+  "process":function(bot, msg, env) {
     var e = msg.content.split(" ").splice(1).join(" ")
-    if(e === ""){
+    if (e === "") {
     var help = "```ruby\nBot Commands:\n"
     var commands = env.general
-    for(var i = 0;i<Object.keys(commands).length;i++){
+    for(var i = 0;i<Object.keys(commands).length;i++) {
       help += `${env.prefix}${commands[Object.keys(commands)[i]].usage} - ${commands[Object.keys(commands)[i]].description}\n`
     }
-    for(var i = 0;i<Object.keys(env.admin).length;i++){
+    for(var i = 0;i<Object.keys(env.admin).length;i++) {
       help += `${env.prefix}${env.admin[Object.keys(env.admin)[i]].usage} - ${env.admin[Object.keys(env.admin)[i]].description}\n`
     }
     help += "\n\nFor more info on a command type "+env.prefix+"help [command name]\n```"
     msg.channel.sendMessage("Sending commands now...").then(function(message){
     msg.author.sendMessage(help).then(function(){
-      message.edit("Commands in your DM's now!")
+      message.edit("My commands have arrived in your DM's :mailbox_with_mail:!")
     })
   })
 }else{
 
-  if(env.general[e]){
+  if (env.general[e]){
     msg.channel.sendMessage(`\`\`\`\n${env.prefix}${env.general[e].usage}\n\n${env.general[e].description}\`\`\``)
   }
-  if(env.admin[e]){
+  if (env.admin[e]){
     msg.channel.sendMessage(`\`\`\`\n${env.prefix}${env.admin[e].usage}\n\n${env.admin[e].description}\`\`\``)
   }
-  if(!env.admin[e] && !env.general[e]){
+  if (!env.admin[e] && !env.general[e]) {
     msg.channel.sendMessage("Sorry I could not find command `"+e+"`")
-  }
-}
+      }
+    }
   }
 }
 
@@ -55,7 +76,7 @@ command.git = {
   "name":"git",
   "usage":"git",
   "description":"Sends a link to the bot's GitHub repo",
-  "process":function(bot,msg,env){
+  "process":function(bot, msg, env) {
     msg.reply("check out my GitHub at https://github.com/Betaaaaa/v10-discord.js-discordbot !")
   }
 }
@@ -64,7 +85,7 @@ command.info = {
   "name":"info",
   "usage":"info",
   "description":"Displays basic information about the bot.",
-  "process":function(bot,msg,env){
+  "process":function(bot, msg, env) {
     msg.reply("Repository created by **Beta ツ#2214**, made for the people.\nDevelopers:\n   Beta ツ#2214\n   ASIANBOI#4122\n   Joseph#5210\nContributors:\n   Marisa Kirisame#7740")
   }
 }
